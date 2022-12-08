644eb1256306d6bd41c277d271215b8635018fe4
user.name <you@example.com>
2022-12-08 18:28:15 +0100

    getting started

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
   // API_URL: 'https://test-email.champiponsi.workers.dev'
+  // API_URL: 'http://localhost:9000
   API_URL: 'https://waxs-and-carts-api.herokuapp.com/api/v1'
 };
