9e68d127cfefb352140634a859f689e3b82b33ea
quality-wax <82378416+quality-wax@users.noreply.github.com>
2023-04-02 03:18:58 -0400

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   // API_URL: 'https://test-email.champiponsi.workers.dev'
   // API_URL: 'http://localhost:9000/api/v1',
-  API_URL: 'https://waxs-and-carts-api.herokuapp.com/api/v1'
+  API_URL: 'https://wax-email-api.osc-fr1.scalingo.io/api/v1'
 };
